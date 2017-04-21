2929f6b460d3f6484c5b58553201149188058c5c
Sean McRoskey <_@_>
2017-04-20 19:50:12 -0700

    Initial Commit

---
+++ ComicStripApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ComicStripApp
+//
+//  Created by Sean McRoskey on 4/20/17.
+//  Copyright © 2017 comicStripClub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
