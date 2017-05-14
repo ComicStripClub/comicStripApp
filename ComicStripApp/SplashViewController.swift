f501783cd1917f7d083c38f93cfe6ed5a63846be
Sethi, Pinkesh <Pinkesh_Sethi2@intuit.com>
2017-05-14 20:35:49 +0530

    1) Animation to splash screen for Button center image and title. 2) Some code refactoring

--- ComicStripApp/SplashViewController.swift
+++ ComicStripApp/SplashViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class SplashViewController: UIViewController {
 
-        @IBOutlet weak var splashImage: UIImageView!
-    @IBOutlet weak var parentUIView: UIView!
+    @IBOutlet weak var splashImage: UIImageView!
+    @IBOutlet weak var imgParentView: UIView!
+    @IBOutlet weak var getStartedButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imgParentView.startRotate()
+        showRotateAnimationWithZoomInOut()
+        getStartedButton.isHidden = true
+        getStartedButton.layer.cornerRadius = 5.0
         // Do any additional setup after loading the view.
     }
 
@@ -22,6 +29,9 @@ class SplashViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        animateTitleLable()
+    }
     @IBAction func onGetStartedClick(_ sender: UIButton) {
         let imgArray = ComicStripPhotoAlbum.sharedInstance.getAllComicsFromAlbum ()
         decideAndLaunchNextFlow(isComicCreated: imgArray.count != 0)
@@ -33,6 +43,51 @@ class SplashViewController: UIViewController {
         }else{
             performSegue(withIdentifier: "firsttimesegue", sender: nil)
         }
+    }
+    
+    func showRotateAnimationWithZoomInOut(){
+        UIView.animate(withDuration: 1.5, animations: {
+            self.imgParentView.transform = CGAffineTransform(scaleX: 8, y: 8)
+        }) { (isZoomOutDone) in
+            if isZoomOutDone{
+                UIView.animate(withDuration: 0.5, animations: {
+                    self.imgParentView.transform = CGAffineTransform(scaleX: 6, y: 6)
+                }, completion: { (isComplete) in
+                    if isComplete{
+                        self.imgParentView.stopRotate()
+                        self.handleButtonVisisblity()
+                        
+                    }
+                })
+            }
+        }
+    }
+    
+    func handleButtonVisisblity(){
+        getStartedButton.isHidden = false
+        getStartedButton.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+        UIView.animate(withDuration: 2.0,
+                       delay: 0,
+                       usingSpringWithDamping: 0.2,
+                       initialSpringVelocity: 6.0,
+                       options: .allowUserInteraction,
+                       animations: { [weak self] in
+                        self?.getStartedButton.transform = .identity
+            },
+                       completion: nil)
+    }
+    
+    func animateTitleLable(){
+        titleLabel.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+        UIView.animate(withDuration: 2.0,
+                       delay: 0,
+                       usingSpringWithDamping: 0.2,
+                       initialSpringVelocity: 6.0,
+                       options: .allowUserInteraction,
+                       animations: { [weak self] in
+                        self?.titleLabel.transform = .identity
+            },
+                       completion: nil)
     }
     /*
     // MARK: - Navigation
